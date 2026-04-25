d497ba6f4248116c010cf73e26d876fa12a5bc17
fudhoil2121 <fudhoil2121@gmail.com>
2026-04-25 09:16:49 +0700

    latihan2_versi2.php
    Signed-off-by: fudhoil2121 <fudhoil2121@gmail.com>

---
+++ pert-2/latihan2_versi2.php
@@ -0,0 +1,16 @@
+<td colspan="3" align="right">Total Harga</td> 
+<td align="right"><?php echo $tharga; ?></td> 
+</tr> 
+<tr> 
+<td colspan="3" align="right"> 
+Diskon <?php echo "( $diskon % )"; ?></td> 
+<td align="right"><?php echo $tdiskon; ?></td> 
+</tr> 
+<tr> 
+<td colspan="3" align="right">Jumlah harus dibayar</td> 
+<td align="right"><?php echo $tdibayar; ?></td> 
+</tr>
+</table> 
+</center> 
+</body> 
+</html>
