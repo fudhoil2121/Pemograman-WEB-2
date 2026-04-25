41b055f1497559974569aea468690c4121f0f4c5
fudhoil2121 <fudhoil2121@gmail.com>
2026-04-25 10:32:36 +0700

    Update latihan1.php
    Signed-off-by: fudhoil2121 <fudhoil2121@gmail.com>

--- pert-3/latihan1.php
+++ pert-3/latihan1.php
@@ -1,2 +1,28 @@
-[InternetShortcut]
-URL=https://github.com/fudhoil2121/pemrograman-web2-pertemuan3/blob/main/latihan1.php
+<?php
+$ball = 'purple'; 
+// Coba ubah ke "yellow", "blue", "green", "purple", atau warna lain
+
+// Inisialisasi variabel agar tidak error "undefined"
+$redbox = $yellowbox = $bluebox = $greenbox = $purplebox = $colorlessbox = '';
+
+if ($ball == 'red') {
+    $redbox = $ball;
+} elseif ($ball == 'yellow') {
+    $yellowbox = $ball;
+} elseif ($ball == 'blue') {
+    $bluebox = $ball;
+} elseif ($ball == 'green') {
+    $greenbox = $ball;
+} elseif ($ball == 'purple') {
+    $purplebox = $ball;
+} else {
+    $colorlessbox = $ball;
+}
+
+echo "red box : $redbox <br>\n";
+echo "yellow box : $yellowbox <br>\n";
+echo "blue box : $bluebox <br>\n";
+echo "green box : $greenbox <br>\n";
+echo "purple box : $purplebox <br>\n";
+echo "colorless box : $colorlessbox <br>\n";
+?>
