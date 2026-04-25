fe06acdb266ba579d5a9082b2dd87d7906d20058
fudhoil2121 <fudhoil2121@gmail.com>
2026-04-25 10:39:46 +0700

    Update latihan1.php
    Signed-off-by: fudhoil2121 <fudhoil2121@gmail.com>

--- pert-6/latihan1.php
+++ pert-6/latihan1.php
@@ -1,2 +1,16 @@
-[InternetShortcut]
-URL=https://github.com/fudhoil2121/pemrograman-web2-pertemuan3/blob/main/latihan1.php
+<?php
+$a[0] =1;
+$a[1] =3;
+$a[2] =5;
+$jumlah =count($a);
+print "Jumlah array a = $jumlah <br>";
+// variabel $jumlah akan bernilai 3
+$b["buah"] = "semangka";
+$b["sayur"] ="wortel";
+$b["daging"] ="ayam";
+$b["utama"] ="nasi";
+$jumlah = sizeof($b);
+print "Jumlah array b = $jumlah <br>";
+// variabel $jumlah akan bernilai 4
+echo "Latihan Array-Muhammad Rifq Fudhoil"
+?>
