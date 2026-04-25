d9758dc3f290ccd9c0595c6b8e7cc14beeb11246
fudhoil2121 <fudhoil2121@gmail.com>
2026-04-25 10:51:52 +0700

    Create sapa.php
    Signed-off-by: fudhoil2121 <fudhoil2121@gmail.com>

---
+++ pert-9/sapa.php
@@ -0,0 +1,9 @@
+<?php
+function sapa($nama) {
+    return "Halo, " . $nama . "!";
+}
+
+echo sapa("Jenna"); // Output: Halo, BabyJenna!
+echo "<br />";
+echo sapa("Nita"); // Output: Halo, SUryanita!
+?>
