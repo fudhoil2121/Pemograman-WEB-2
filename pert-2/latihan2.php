8d9f07c5b4194308b63472891eb052d2cac577c2
fudhoil2121 <fudhoil2121@gmail.com>
2026-04-25 09:00:29 +0700

    latihan2.php

---
+++ pert-2/latihan2.php
@@ -0,0 +1,27 @@
+<html> 
+<head> 
+<title>Daftar Peralatan Yang Dibeli</title> 
+</head> 
+<style TYPE="text/css">
+  body {
+	  font-size: 14pt;
+  }
+  table {
+	font-size: 25pt;
+  }
+ </style>
+<body> 
+<center> 
+<font face="comic sans serif" size=5 color= "blue">Contoh Perhitungan dengan PHP</font>
+<table border="1" cellspacing="0" cellpadding="3"> 
+<tr> 
+<td colspan="4" align="center" valign="middle"> 
+<b>Daftar Pemesanan Peralatan Kantor</b> 
+</td> 
+</tr> 
+<tr> 
+<td><b>Nama Peralatan</b></td> 
+<td><b>Jumlah</b></td> 
+<td><b>Harga Satuan</b></td> 
+<td><b>Jumlah Harga</b></td> 
+</tr> 
