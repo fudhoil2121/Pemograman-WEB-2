606cb343385669269dd1cc4826d7a7dcd533a6f5
fudhoil2121 <fudhoil2121@gmail.com>
2026-04-25 10:40:42 +0700

    Create latihan2.php
    Signed-off-by: fudhoil2121 <fudhoil2121@gmail.com>

---
+++ pert-6/latihan2.php
@@ -0,0 +1,19 @@
+<html>
+    <body>
+        <?php
+        $x = array("one", "two", "three");
+
+        foreach ($x as $value) {
+            echo $value . "<br />";
+        }
+
+        $b["sayur"] = "wortel";
+        $b["daging"] = "ayam";
+        $b["utama"] = "nasi";
+
+        $jumlah = sizeof($b);
+        print "Jumlah array b = $jumlah <br>"; // hasilnya 3
+        echo "Latihan Array-Muhammad Rifqi Fudhoil"
+        ?>        
+    </body>
+</html>
