f8629d69e58836a3fb4a21fdf094ed1ad6197e56
fudhoil2121 <fudhoil2121@gmail.com>
2026-04-25 10:47:58 +0700

    Create latihan_string.php
    Signed-off-by: fudhoil2121 <fudhoil2121@gmail.com>

---
+++ pert-9/latihan_string.php
@@ -0,0 +1,23 @@
+<?php
+$teks = "pemrograman web";
+
+// strlen
+echo "Jumlah karakter: " . strlen($teks) . "<br>";
+
+// strtoupper
+echo "Huruf besar: " . strtoupper($teks) . "<br>";
+
+// strtolower
+echo "Huruf kecil: " . strtolower("PEMROGRAMAN") . "<br>";
+
+// ucfirst
+echo "Huruf pertama besar: " . ucfirst($teks) . "<br>";
+
+// ucwords
+echo "Huruf pertama tiap kata besar: " . ucwords("pemrograman web lanjutan") . "<br>";
+
+// ltrim & rtrim
+echo "Ltrim: '" . ltrim("   teks") . "'<br>";
+echo "Rtrim: '" . rtrim("teks   ") . "'<br>";
+
+?>
