cb265882113d4062bb2b07ab0d4202bbe80726b5
fudhoil2121 <fudhoil2121@gmail.com>
2026-04-25 09:17:43 +0700

    Create latihan2b.php
    Signed-off-by: fudhoil2121 <fudhoil2121@gmail.com>

---
+++ pert-2/latihan2b.php
@@ -0,0 +1,28 @@
+<?php 
+// Mulai untuk mengisi tabel daftar dengan data yang ada 
+?> 
+<tr> 
+<td align="left"><?php echo $brg1; ?></td> 
+<td align="right"><?php echo $jmlbrg1; ?></td> 
+<td align="right"><?php echo $harga1; ?></td> 
+<td align="right"><?php echo $th1; ?></td> 
+</tr> 
+<tr> 
+<td align="left"><?php echo $brg2; ?></td> 
+<td align="right"><?php echo $jmlbrg2; ?></td> 
+<td align="right"><?php echo $harga2; ?></td> 
+<td align="right"><?php echo $th2; ?></td> 
+</tr> 
+<tr> 
+<td align="left"><?php echo $brg3; ?></td> 
+<td align="right"><?php echo $jmlbrg3; ?></td> 
+<td align="right"><?php echo $harga3; ?></td> 
+<td align="right"><?php echo $th3; ?></td> 
+</tr> 
+<tr> 
+<td align="left"><?php echo $brg4; ?></td> 
+<td align="right"><?php echo $jmlbrg4; ?></td> 
+<td align="right"><?php echo $harga4; ?></td> 
+<td align="right"><?php echo $th4; ?></td> 
+</tr> 
+<tr> 
