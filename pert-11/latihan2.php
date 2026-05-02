251224940051ca3738d1f3684e48b01cdd7a0c5e
fudhoil2121 <fudhoil2121@gmail.com>
2026-05-02 10:58:55 +0700

    Create latihan2.php
    Signed-off-by: fudhoil2121 <fudhoil2121@gmail.com>

---
+++ pert-11/latihan2.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+
+<form action="proses.php" method="post">
+Nama: <input type="text" name="nama"><br>
+Email: <input type="text" name="email"><br>
+Pesan: <textarea name="pesan"></textarea><br>
+<input type="submit" value="Kirim">
+</form>
+
+</body>
+</html>
